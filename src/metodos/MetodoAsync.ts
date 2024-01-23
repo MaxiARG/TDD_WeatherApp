d9f8f1712ae7e147b4a67765cb12f232a2eab8bd
MaxiARG <maxiramos2017@gmail.com>
2024-01-23 08:35:14 -0300

    inicio

--- src/metodos/MetodoAsync.ts
+++
@@ -1,9 +0,0 @@
-const metodoAsincronico = (valor : number) => {
-    return new Promise((resolve, reject) => {
-        if(valor>1)
-            return resolve(1);
-        return reject(2);
-    })
-}
-
-export default metodoAsincronico;
--- src/metodos/_forEach.ts
+++
@@ -1,5 +0,0 @@
-export function _forEach(items, callback) {
-    for (const item of items) {
-      callback(item);
-    }
-  }
